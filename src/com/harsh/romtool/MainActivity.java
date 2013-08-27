eb45006b108cc4c6257d7343fb33442159ce8d2d
Harsh Panchal <harsh.dude16@gmail.com>
2013-08-27 18:09:29 +0530

    Version 1.1 - Add init.d support for setting up CRT Animation at bootup (via init.d script)

--- src/com/harsh/romtool/MainActivity.java
+++ src/com/harsh/romtool/MainActivity.java
@@ -1,10 +1,16 @@
 package com.harsh.romtool;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.Settings;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -73,15 +79,20 @@ public class MainActivity extends Activity {
 			        		try {
 								Runtime.getRuntime().exec(new String[] { "su", "-c", "echo 1 > ", FBDELAY });
 								Runtime.getRuntime().exec(new String[] { "su", "-c", "echo 350 > ", FBDELAY_MS });
+								mountSystemRW();
+								copyAssets("03_crt");
 							} catch (IOException e) {
 								Toast.makeText(getApplicationContext(), "No SU Rights", Toast.LENGTH_SHORT).show();
-							} 
+								Log.e("harsh_debug","Failed to get SU Rights or Unsupported Kernel");
+							}
 			        	} else {
 			        		Settings.System.putInt(getContentResolver(), CRT_ANIM,0);
 			        		Log.d("harsh_debug","harsh_crt=>0");
 			        		try {
 			        			Runtime.getRuntime().exec(new String[] { "su", "-c", "echo 0 > ", FBDELAY });
 			        			Runtime.getRuntime().exec(new String[] { "su", "-c", "echo 0 > ", FBDELAY_MS });
+			        			mountSystemRW();
+			        			copyAssets("99_crtoff");
 							} catch (IOException e) {
 								Toast.makeText(getApplicationContext(), "No SU Rights or Unsupported Kernel", Toast.LENGTH_SHORT).show();
 								Log.e("harsh_debug","Failed to get SU Rights or Unsupported Kernel");
@@ -161,6 +172,62 @@ public class MainActivity extends Activity {
         default:
         return super.onOptionsItemSelected(item);
     }
+    }
+    
+    public void copyAssets(String script) {
+        AssetManager assetManager = getAssets();
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+              in = assetManager.open(script);
+              File outFile = new File(Environment.getExternalStorageDirectory().getPath(), script);
+              out = new FileOutputStream(outFile);
+              copyFile(in, out);
+              in.close();
+              in = null;
+              out.flush();
+              out.close();
+              out = null;
+            } catch(IOException e) {
+                Log.e("harsh_debug", "Failed to handle: " + script, e);
+            }
+            try {
+				Runtime.getRuntime().exec(new String[] { "su", "-c", "cp", Environment.getExternalStorageDirectory().getPath()+"/"+script, "/system/etc/init.d/"+script });
+				Runtime.getRuntime().exec(new String[] { "su", "-c", "rm", Environment.getExternalStorageDirectory().getPath()+"/"+script });
+			} catch (IOException e) {
+				Log.e("harsh_debug", "Failed to move: " + script, e);
+			}
+            if(script=="03_crt" || script=="99_crtoff"){ 
+            	if(script=="03_crt") {
+            		try {
+						Runtime.getRuntime().exec(new String[] { "su", "-c", "rm", "/system/etc/init.d/99_crtoff" });
+					} catch (IOException e) {
+						Log.e("harsh_debug", "Failed to remove: " + script, e);
+					}
+            	}
+            	else {
+            		try {
+						Runtime.getRuntime().exec(new String[] { "su", "-c", "rm", "/system/etc/init.d/03_crt" });
+					} catch (IOException e) {
+						Log.e("harsh_debug", "Failed to remove: " + script, e);
+					}
+            	}
+            }
+    }
+    public void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+          out.write(buffer, 0, read);
+        }
+    }
+    
+    public void mountSystemRW() {
+    	try {
+			Runtime.getRuntime().exec(new String[] { "su", "-c", "mount", "-o", "remount,rw", "/dev/block/mmcblk0p3", "/system" });
+		} catch (IOException e) {
+			Log.e("harsh_debug", "Failed to mount system as R/W", e);
+		}
     }
     
 }
