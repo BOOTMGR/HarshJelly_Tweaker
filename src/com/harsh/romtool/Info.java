11c796ee91c1c116526322f5bd19c2a1d7fba168
Harsh Panchal <harsh.dude16@gmail.com>
2013-12-31 08:47:34 +0530

    Version 4.2 - Statusbar transparency chooser - Allow user to set username - Bug fixes and optimizations

--- src/com/harsh/romtool/Info.java
+++ src/com/harsh/romtool/Info.java
@@ -1,7 +1,11 @@
-/*
- *      HarshJelly Tweaker - An app to Tweak HarshJelly ROM
- *      Author : Harsh Panchal <panchal.harsh18@gmail.com, mr.harsh@xda-developers.com>
- */
+/*******************************************************************************
+ * Copyright (c) 2013 "Harsh Panchal" <panchal.harsh18@gmail.com>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ ******************************************************************************/
+
 package com.harsh.romtool;
 
 import android.app.Activity;
--- src/com/harsh/romtool/Xposed_ClearDefaults.java
+++ src/com/harsh/romtool/Xposed_ClearDefaults.java
@@ -1,8 +1,11 @@
-/*
- *      HarshJelly Tweaker - An app to Tweak HarshJelly ROM
- *      Taken from : https://github.com/MohammadAG/Xposed-Disable-Clear-Defaults-Dialog
- *      Original Author : MohammadAG
- */
+/*******************************************************************************
+ * Copyright (c) 2013 "Harsh Panchal" <panchal.harsh18@gmail.com>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ ******************************************************************************/
+
 package com.harsh.romtool;
 
 import android.app.Activity;
